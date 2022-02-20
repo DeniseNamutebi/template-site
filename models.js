a9f519c915688fdd30f1612eaa689d14b837b17b
Denise Namutebi <denisenamutebi@gmail.com>
2022-02-20 00:58:55 +0000

    formatting files

--- models.js
+++ models.js
@@ -1,43 +1,49 @@
-const {Sequelize, Model, DataTypes} = require('sequelize')
-const path = require('path')
-const sequelize = process.env.NODE_ENV === 'test'
-    ? new Sequelize('sqlite::memory:', null, null, {dialect: 'sqlite'})
-    : new Sequelize({dialect: 'sqlite', storage: path.join(__dirname, 'data.db')})
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const path = require("path");
+const sequelize =
+  process.env.NODE_ENV === "test"
+    ? new Sequelize("sqlite::memory:", null, null, { dialect: "sqlite" })
+    : new Sequelize({
+        dialect: "sqlite",
+        storage: path.join(__dirname, "data.db"),
+      });
 
-
-
-    
 class Restaurant extends Model {}
-Restaurant.init({
+Restaurant.init(
+  {
     name: DataTypes.STRING,
-    image: DataTypes.STRING
-}, {sequelize, modelName: 'restaurant'})
-
+    image: DataTypes.STRING,
+  },
+  { sequelize, modelName: "restausrant" }
+);
 
 class Menu extends Model {}
-Menu.init({
+Menu.init(
+  {
     title: DataTypes.STRING,
-    image: DataTypes.STRING
-}, {sequelize, modelName: 'menu'})
-
+    image: DataTypes.STRING,
+  },
+  { sequelize, modelName: "menu" }
+);
 
 class Item extends Model {}
-Item.init({
+Item.init(
+  {
     name: DataTypes.STRING,
     price: DataTypes.FLOAT,
-    image: DataTypes.STRING
-},{sequelize, modelName:"item"})
+    image: DataTypes.STRING,
+  },
+  { sequelize, modelName: "item" }
+);
 
-
-Restaurant.hasMany(Menu,{as: "menus"})
-Menu.belongsTo(Restaurant)
-Menu.hasMany(Item, {as: 'items'})
-Item.belongsTo(Menu)
-
+Restaurant.hasMany(Menu, { as: "menus" });
+Menu.belongsTo(Restaurant);
+Menu.hasMany(Item, { as: "items" });
+Item.belongsTo(Menu);
 
 module.exports = {
-    Restaurant,
-    Menu,
-    Item,
-    sequelize
-}
+  Restaurant,
+  Menu,
+  Item,
+  sequelize,
+};
